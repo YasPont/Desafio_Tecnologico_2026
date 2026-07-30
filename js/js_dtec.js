04ef3787727ff687c89cfa7f9cef906cf83e3af5
marcos-bc <marcos.bertolini@alu.ufc.br>
2026-07-30 16:49:34 -0300

    Update js_dtec.js

--- js/js_dtec.js
+++ js/js_dtec.js
@@ -21,43 +21,52 @@ document.addEventListener("DOMContentLoaded", function() {
   map.fitBounds(bounds);
 
   // 3. Grupos de Camadas para possibilitar os Filtros
-  var estandesGroup = L.layerGroup().addTo(map);
-  var auditoriosGroup = L.layerGroup().addTo(map);
+  //var estandesGroup = L.layerGroup().addTo(map);
+  //var auditoriosGroup = L.layerGroup().addTo(map);
 
   // 4. Mapear Estandes (Polígonos clicáveis sobre a imagem)
-  var estande1 = L.polygon([
-    [350, 400], [350, 650], [550, 650], [550, 400]
-  ], { 
-    color: '#E854C9', 
-    fillColor: '#E854C9', 
-    fillOpacity: 0.4,
-    weight: 2 
-  }).addTo(estandesGroup);
-  estande1.bindPopup("<b>Estande Principal</b><br>Exposição de Projetos RAITec");
+  //var estande1 = L.polygon([
+   // [350, 400], [350, 650], [550, 650], [550, 400]
+ // ], { 
+   // color: '#E854C9', 
+    //fillColor: '#E854C9', 
+   // fillOpacity: 0.4,
+   // weight: 2 
+  //}).addTo(estandesGroup);
+ // estande1.bindPopup("<b>Estande Principal</b><br>Exposição de Projetos RAITec");
 
   // 5. Mapear Auditórios
-  var auditorio1 = L.polygon([
-    [700, 800], [700, 1150], [950, 1150], [950, 800]
-  ], { 
-    color: '#7B2FF7', 
-    fillColor: '#7B2FF7', 
-    fillOpacity: 0.4,
-    weight: 2 
-  }).addTo(auditoriosGroup);
-  auditorio1.bindPopup("<b>Auditório Principal</b><br>Palcos de Palestras e Abertura");
+  //var auditorio1 = L.polygon([
+    //[700, 800], [700, 1150], [950, 1150], [950, 800]
+  //], { 
+//    color: '#7B2FF7', 
+//    fillColor: '#7B2FF7', 
+//    fillOpacity: 0.4,
+    //weight: 2 
+//  }).addTo(auditoriosGroup);
+  //auditorio1.bindPopup("<b>Auditório Principal</b><br>Palcos de Palestras e Abertura");
 
   // 6. Função para acionar os filtros pelos botões na tela
-  window.filtrarMapa = function(categoria) {
-    map.removeLayer(estandesGroup);
-    map.removeLayer(auditoriosGroup);
+//  window.filtrarMapa = function(categoria) {
+  //  map.removeLayer(estandesGroup);
+  //  map.removeLayer(auditoriosGroup);
 
-    if (categoria === 'todos') {
-      map.addLayer(estandesGroup);
-      map.addLayer(auditoriosGroup);
-    } else if (categoria === 'estandes') {
-      map.addLayer(estandesGroup);
-    } else if (categoria === 'auditorios') {
-      map.addLayer(auditoriosGroup);
-    }
-  };
+  //  if (categoria === 'todos') {
+  //    map.addLayer(estandesGroup);
+  //    map.addLayer(auditoriosGroup);
+  //  } else if (categoria === 'estandes') {
+ //     map.addLayer(estandesGroup);
+ //   } else if (categoria === 'auditorios') {
+  //    map.addLayer(auditoriosGroup);
+   // }
+ // };
+  // FERRAMENTA DE DESENVOLVEDOR: Descobrir coordenadas ao clicar
+  map.on('click', function(e) {
+    var y = Math.round(e.latlng.lat);
+    var x = Math.round(e.latlng.lng);
+    
+    // Mostra um aviso na tela com as coordenadas exatas
+    alert("Coordenada: [" + y + ", " + x + "]");
+    console.log("Coordenada copiada: [" + y + ", " + x + "]");
+  });
 });
