15cb91489218d0529b9d43f84d24a6d63cffabec
marcos-bc <marcos.bertolini@alu.ufc.br>
2026-07-30 17:57:16 -0300

    Refactor map initialization and markers

--- js/js_dtec.js
+++ js/js_dtec.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // 1. Inicializar o mapa na div #mapa-interativo (coordenadas de imagem)
   var map = L.map('mapa-interativo', {
     crs: L.CRS.Simple,
-    minZoom: -2,
+    minZoom: -1,
     maxZoom: 2,
     zoomControl: true,
     attributionControl: false
